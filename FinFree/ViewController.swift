bfe01a4b8bce24839f81aa07ab663bf94309be3a
Bjoern Kaczmarek <bjoern.kaczmarek@gmail.com>
2018-11-24 20:29:24 +1100

    Initial Commit

---
+++ FinFree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinFree
+//
+//  Created by Björn Kaczmarek on 24/11/18.
+//  Copyright © 2018 Björn Kaczmarek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
